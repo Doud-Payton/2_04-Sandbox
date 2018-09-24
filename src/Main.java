0c9fa9614597bf030e5af0574706a5774ca493c7
Student <19payton.do@student.sfsd.us>
2018-09-24 08:11:00 -0700

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String firstname = "Payton";
+		String lastname = "Doud";
+		System.out.println(firstname + " " + lastname);
+
+	}
+
+}
